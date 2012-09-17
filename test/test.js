d58bb169ed1e2359ae4d8ebe22b3b8624b190c2f
David Brockman Smoliansky <david.bs@gmail.com>
2012-09-17 22:30:28 +0200

    first commit

---
+++ test/test.js
@@ -0,0 +1,54 @@
+var assert = require('assert');
+var Set = require('../index');
+
+describe('static functions', function(){
+	
+	describe('create()', function(){
+		it('should return an empty object', function(){
+			var o = Set.create();
+			assert.strictEqual('object', typeof o);
+			var i = 0;
+			for (var k in o) i++;
+			assert.strictEqual(0, i);
+			assert.strictEqual(undefined, o.hasOwnProperty);
+		});
+	});
+
+	describe('has()', function(){
+		it('should return an empty object', function(){
+			var o = Set.create();
+			assert.strictEqual(false, Set.has(o, 'foo'));
+			Set.add(o, 'foo');
+			assert.strictEqual(true, Set.has(o, 'foo'));
+			o.foo = null;
+			assert.strictEqual(true, Set.has(o, 'foo'));
+			delete o.foo;
+			assert.strictEqual(false, Set.has(o, 'foo'));
+		});
+	});
+
+	describe('add()', function(){
+		it('should return an empty object', function(){
+			var o = Set.create();
+			assert.strictEqual(false, Set.has(o, 'foo'));
+			var added = Set.add(o, 'foo');
+			assert.strictEqual(true, Set.has(o, 'foo'));
+			assert.strictEqual(true, added);
+			added = Set.add(o, 'foo');
+			assert.strictEqual(false, added);
+		});
+	});
+
+	describe('remove()', function(){
+		it('should return an empty object', function(){
+			var o = Set.create();
+			assert.strictEqual(false, Set.has(o, 'foo'));
+			var added = Set.add(o, 'foo');
+			assert.strictEqual(true, Set.has(o, 'foo'));
+			assert.strictEqual(true, added);
+			added = Set.add(o, 'foo');
+			assert.strictEqual(false, added);
+		});
+	});
+
+});
