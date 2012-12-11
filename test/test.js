03622ccdf0db9db3bfd6a3aa41e77c056700d969
David Brockman Smoliansky <david.bs@gmail.com>
2012-12-11 01:54:18 +0100

    remove junk

--- test/test.js
+++ test/test.js
@@ -53,7 +53,7 @@ describe('Set.addSet(A, B)', function(){
 		var a = Set.create();
 
 		//TODO: Add manually
-		var b = Set.from(['a', 'b', 'c', 'b', 'a']);
+		var b = Set.create(['a', 'b', 'c', 'b', 'a']);
 
 		var count = Set.addSet(a, b);
 		assert.strictEqual(3, count);
@@ -74,8 +74,8 @@ describe('Set.remove(set, item)', function(){
 
 describe('Set.removeSet(A, B)', function(){
 	it('should modify the set to contain only items that are present in that object and in the other set', function(){
-		var a = Set.from(['a', 'b', 'c', 'd', 'e', 'f', 'x']);
-		var b = Set.from(['d', 'e', 'f', 'y', 'z']);
+		var a = Set.create(['a', 'b', 'c', 'd', 'e', 'f', 'x']);
+		var b = Set.create(['d', 'e', 'f', 'y', 'z']);
 		var count = Set.removeSet(a, b);
 		assert.strictEqual(3, count, 'removed 3 items');
 		assert.strictEqual(4, Set.size(a), 'size is now 4');
@@ -86,8 +86,8 @@ describe('Set.removeSet(A, B)', function(){
 
 describe('Set.symmetricRemoveSet(A, B)', function(){
 	it('should modify the set to contain only elements that are present either in A or in B, but not both', function(){
-		var o = Set.from(['a', 'b', 'c', 'd', 'e', 'f', 'x']);
-		var o2 = Set.from(['d', 'e', 'f', 'y', 'z']);
+		var o = Set.create(['a', 'b', 'c', 'd', 'e', 'f', 'x']);
+		var o2 = Set.create(['d', 'e', 'f', 'y', 'z']);
 		Set.symmetricRemoveSet(o, o2);
 		assert.strictEqual(6, Set.size(o), 'size is now 4');
 		var s = Set.toArray(o).sort().join(',');
@@ -95,9 +95,9 @@ describe('Set.symmetricRemoveSet(A, B)', function(){
 	});
 });
 
-describe('Set.from([...])', function(){
+describe('Set.create([...])', function(){
 	it('should  create a new object and add all unique items in an array', function(){
-		var o = Set.from(['a', 'b', 'c', 'b', 'a']);
+		var o = Set.create(['a', 'b', 'c', 'b', 'a']);
 		var arr = Set.toArray(o);
 		assert.strictEqual(3, arr.length);
 		assert.strictEqual('a,b,c', arr.sort().join(','));
@@ -106,7 +106,7 @@ describe('Set.from([...])', function(){
 
 describe('Set.clear(set)', function(){
 	it('should clear the set', function(){
-		var o = Set.from(['a', 'b', 'c', 'd', 'e', 'f']);
+		var o = Set.create(['a', 'b', 'c', 'd', 'e', 'f']);
 		Set.clear(o);
 		var arr = Set.toArray(o);
 		assert.strictEqual(0, arr.length);
@@ -125,22 +125,10 @@ describe('Set.size(set)', function(){
 	});
 });
 
-describe('Set.clone(set)', function(){
-	it('should return a clone of the set', function(){
-		var orig = Set.from(['a', 'b', 'c']);
-		var clone = Set.clone(orig);
-		assert.notStrictEqual(orig, clone, 'not the same ref');
-		assert.strictEqual(Set.size(orig), Set.size(clone), 'same size');
-		var s1 = Set.toArray(orig).sort().join(',');
-		var s2 = Set.toArray(clone).sort().join(',');
-		assert.strictEqual(s1, s2, 'same items');
-	});
-});
-
 describe('Set.intersectWith(A, B)', function(){
 	it('should modify A to contain only items that are present in A and in B', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['b', 'c', 'd']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['b', 'c', 'd']);
 		Set.intersectWith(a, b);
 		assert.strictEqual(2, Set.size(a));
 		var s = Set.toArray(a).sort().join(',');
@@ -150,21 +138,21 @@ describe('Set.intersectWith(A, B)', function(){
 
 describe('Set.overlaps(A, B)', function(){
 	it('should return true if the two sets share common elements', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['x', 'a', 'y']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['x', 'a', 'y']);
 		assert.strictEqual(true, Set.overlaps(a, b));
 	});
 	it('should return false if the two sets share no common elements', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['d', 'e', 'f']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['d', 'e', 'f']);
 		assert.strictEqual(false, Set.overlaps(a, b));
 	});
 });
 
 describe('Set.equals(A, B)', function(){
 	it('should return true if the two sets have the same number of elements and contain the same elements', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['c', 'a', 'b']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['c', 'a', 'b']);
 		assert.strictEqual(true, Set.equals(a, b));
 	});
 	it('should return true if both A and B are empty', function(){
@@ -173,18 +161,18 @@ describe('Set.equals(A, B)', function(){
 		assert.strictEqual(true, Set.equals(a, b));
 	});
 	it('should return false if the one or more elements differ', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'b', 'x']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'x']);
 		assert.strictEqual(false, Set.equals(a, b));
 	});
 	it('should return false if A has elements not found in B', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'b', 'c', 'x']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'c', 'x']);
 		assert.strictEqual(false, Set.equals(a, b));
 	});
 	it('should return false if B has elements not found in A', function(){
-		var a = Set.from(['a', 'b', 'c', 'x']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c', 'x']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(false, Set.equals(a, b));
 	});
 });
@@ -192,7 +180,7 @@ describe('Set.equals(A, B)', function(){
 describe('Set.isSubsetOf(A, B)', function(){
 	it('should return true if A is empty', function(){
 		var a = Set.create();
-		var b = Set.from(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(true, Set.isSubsetOf(a, b));
 	});
 	it('should return true if A is empty, even if B is empty', function(){
@@ -201,18 +189,18 @@ describe('Set.isSubsetOf(A, B)', function(){
 		assert.strictEqual(true, Set.isSubsetOf(a, b));
 	});
 	it('should return true if the sets are equal', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(true, Set.isSubsetOf(a, b));
 	});
 	it('should return false if A is larger', function(){
-		var a = Set.from(['a', 'b', 'c', 'd']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c', 'd']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(false, Set.isSubsetOf(a, b));
 	});
 	it('should return false if A contains an elements not found in B', function(){
-		var a = Set.from(['x']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['x']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(false, Set.isSubsetOf(a, b));
 	});
 });
@@ -220,12 +208,12 @@ describe('Set.isSubsetOf(A, B)', function(){
 describe('Set.isProperSubsetOf(A, B)', function(){
 	it('should return true if A is empty', function(){
 		var a = Set.create();
-		var b = Set.from(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(true, Set.isProperSubsetOf(a, b));
 	});
 	it('should return true if A contains some of the elements in B but not all', function(){
-		var a = Set.from(['a', 'b']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(true, Set.isProperSubsetOf(a, b));
 	});
 	it('should return false if both A and B are empty', function(){
@@ -234,25 +222,25 @@ describe('Set.isProperSubsetOf(A, B)', function(){
 		assert.strictEqual(false, Set.isProperSubsetOf(a, b));
 	});
 	it('should return false if the sets are equal', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(false, Set.isProperSubsetOf(a, b));
 	});
 	it('should return false if A is larger', function(){
-		var a = Set.from(['a', 'b', 'c', 'd']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c', 'd']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(false, Set.isProperSubsetOf(a, b));
 	});
 	it('should return false if A contains an elements not found in B', function(){
-		var a = Set.from(['x']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['x']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(false, Set.isProperSubsetOf(a, b));
 	});
 });
 
 describe('Set.isSupersetOf(A, B)', function(){
 	it('should return true if B is empty', function(){
-		var a = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c']);
 		var b = Set.create();
 		assert.strictEqual(true, Set.isSupersetOf(a, b));
 	});
@@ -262,31 +250,31 @@ describe('Set.isSupersetOf(A, B)', function(){
 		assert.strictEqual(true, Set.isSupersetOf(a, b));
 	});
 	it('should return true if the sets are equal', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(true, Set.isSupersetOf(a, b));
 	});
 	it('should return false if B is larger', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'b', 'c', 'd']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'c', 'd']);
 		assert.strictEqual(false, Set.isSupersetOf(a, b));
 	});
 	it('should return false if B contains an elements not found in A', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'x']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'x']);
 		assert.strictEqual(false, Set.isSupersetOf(a, b));
 	});
 });
 
 describe('Set.isProperSupersetOf(A, B)', function(){
 	it('should return true if B is empty', function(){
-		var a = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c']);
 		var b = Set.create();
 		assert.strictEqual(true, Set.isProperSupersetOf(a, b));
 	});
 	it('should return true if A is a proper superset of B', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'b']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b']);
 		assert.strictEqual(true, Set.isProperSupersetOf(a, b));
 	});
 	it('should return false if both A and B are empty', function(){
@@ -295,26 +283,26 @@ describe('Set.isProperSupersetOf(A, B)', function(){
 		assert.strictEqual(false, Set.isProperSupersetOf(a, b));
 	});
 	it('should return false if the sets are equal', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(false, Set.isProperSupersetOf(a, b));
 	});
 	it('should return false if B is larger', function(){
-		var a = Set.from(['a', 'b']);
-		var b = Set.from(['a', 'b', 'c']);
+		var a = Set.create(['a', 'b']);
+		var b = Set.create(['a', 'b', 'c']);
 		assert.strictEqual(false, Set.isProperSupersetOf(a, b));
 	});
 	it('should return false if B contains an elements not found in A', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['a', 'x']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['a', 'x']);
 		assert.strictEqual(false, Set.isProperSupersetOf(a, b));
 	});
 });
 
 describe('Set.union(A, B)', function(){
 	it('should return the set of all elements that are a member of A, or B, or both', function(){
-		var a = Set.from(['a', 'b', 'c']);
-		var b = Set.from(['b', 'c', 'd']);
+		var a = Set.create(['a', 'b', 'c']);
+		var b = Set.create(['b', 'c', 'd']);
 		var c = Set.union(a, b);
 		var arr = Set.toArray(c);
 		assert.strictEqual(4, arr.length);
